c8bb08fa9d18f9cf2d29c6aeb848f3db319ad200
Seong Kim <hoon5421@hotmail.com>
2018-04-16 20:00:06 -0400

    Updating css for project views

--- src/components/projects/project.js
+++ src/components/projects/project.js
@@ -5,7 +5,11 @@ class Project extends Component {
     render() {
         return (
             <div className="project">
-            <img className="thumbnail" src={process.env.PUBLIC_URL + '/images/bearfish.png'} alt="test"/>
+            <img className="thumbnail" src={process.env.PUBLIC_URL + '/images/bearfish.png'} alt="test"/>            
+            <div className="title">
+              <h1 className="mainTitle">Test</h1>
+              <p className="subTitle">Here goes Subtitle</p>
+            </div>
           </div>
         );
       }
