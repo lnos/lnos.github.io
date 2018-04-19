2bf12eed280faf382ad1d9cfd198750ee17668da
Seong Kim <hoon5421@hotmail.com>
2018-04-19 18:29:14 -0400

    Updating css for project layout

--- src/components/projects/project.js
+++ src/components/projects/project.js
@@ -4,11 +4,13 @@ import './project.css';
 class Project extends Component {
     render() {
         return (
+          <div className="container">
             <div className="project">
-            <img className="thumbnail" src={process.env.PUBLIC_URL + '/images/bearfish.png'} alt="test"/>            
-            <div className="title">
-              <h1 className="mainTitle">Test</h1>
-              <p className="subTitle">Here goes Subtitle</p>
+              <img className="thumbnail" src={process.env.PUBLIC_URL + '/images/bearfish.png'} alt="test"/>            
+              <div className="title">
+                <h2 className="mainTitle">Bear Fish</h2>
+                <p className="subTitle">Infinite Sidescroller with Retro Graphics and cool item mechanics.</p>
+              </div>
             </div>
           </div>
         );
