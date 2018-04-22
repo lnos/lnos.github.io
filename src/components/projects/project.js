d3053ef31f1771bf7423ba1b871f2398b97dcd3a
Seong Kim <hoon5421@hotmail.com>
2018-04-22 14:05:48 -0400

    Generating HTML contents with function with son data

--- src/components/projects/project.js
+++ src/components/projects/project.js
@@ -6,11 +6,13 @@ class Project extends Component {
         return (
           <div className="container">
             <div className="project">
-              <img className="thumbnail" src={process.env.PUBLIC_URL + '/images/bearfish.png'} alt="test"/>            
+              <a href={this.props.link}>
+              <img className="thumbnail" src={process.env.PUBLIC_URL + '/images/' + this.props.thumbnail} alt={this.props.title}/>            
               <div className="title">
-                <h2 className="mainTitle">Bear Fish</h2>
-                <p className="subTitle">Infinite Sidescroller with Retro Graphics and cool item mechanics.</p>
+                <h2 className="mainTitle">{this.props.title}</h2>
+                <p className="subTitle">{this.props.subtitle}</p>
               </div>
+              </a>
             </div>
           </div>
         );
--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
 import Project from './project.js';
 import './projects.css';
+import ProjectData from './projects.json'
 
 class Projects extends Component {
 
     getProjects(){
 
+      var projectArray = [];
+      for (var i = 0; i < ProjectData.length; i++){
+        projectArray.push(
+          <Project
+          title ={ProjectData[i].title}
+          subtitle={ProjectData[i].subtitle}
+          thumbnail={ProjectData[i].thumbnail}
+          link = {ProjectData[i].link}
+        />
+        );
+      }
+      return projectArray;
     }
 
     render() {
+        var projectData = this.getProjects()
+        console.log(projectData);
         return (
           <div className="projects">
             <h3 className="projects-title">Works</h3>
             <div className="border"></div>
             <div className="projects-contents">
-              <Project/>
-              <Project/>
-              <Project/>
-              <Project/>
-              <Project/>
-              <Project/>
+              {projectData}
             </div>
           </div>
         );
